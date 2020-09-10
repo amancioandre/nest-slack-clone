ee0452bf36c7645bfde2591a1f6b80f5f81780ae
André Moraes <amancioandre@gmail.com>
2020-09-10 19:04:31 -0300

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { SlackGateway } from './slack/slack.gateway';
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-slack-clone'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest-slack-clone'), UserModule, AuthModule],
   controllers: [],
   providers: [SlackGateway],
 })
---
+++ src/auth/dto/signup-payload.dto.ts
@@ -0,0 +1,5 @@
+export class SignUpPayloadDTO {
+    name: string
+    username: string
+    password: string
+}
